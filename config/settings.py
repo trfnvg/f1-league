4c90a32b5fd145b2d43cd58ba1149d4b0fa2d0ac
Grigorii Trifonov <grisha.trifonov@yandex.ru>
2026-02-19 00:20:39 +0300

    add docker

--- config/settings.py
+++ config/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+import os, dj_database_url
+
+DATABASES = {
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
