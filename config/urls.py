0b04babc01835d9380aa8bef3808cab5b62f0eed
Grigorii Trifonov <grisha.trifonov@yandex.ru>
2026-02-22 02:10:08 +0300

    1

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/login/", auth_views.LoginView.as_view(template_name="registration/login_v2.html"), name="login"),
     path("accounts/", include("django.contrib.auth.urls")),  # ← добавили
     path("", include("league.urls")),
 ]
