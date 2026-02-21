c60d08b21ab636eefc15a7f7c08b8df91b7b7a95
Grigorii Trifonov <grisha.trifonov@yandex.ru>
2026-02-22 02:21:06 +0300

    1

--- league/admin.py
+++ league/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
+from django import forms
 from .scoring import calculate_event_scores
 from .models import Event, EventPhoto, Prediction, Result, Score
+
+
+class EventAdminForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = "__all__"
+        widgets = {
+            "deadline": forms.DateTimeInput(
+                attrs={"type": "datetime-local"},
+                format="%Y-%m-%dT%H:%M",
+            ),
+            "race_datetime": forms.DateTimeInput(
+                attrs={"type": "datetime-local"},
+                format="%Y-%m-%dT%H:%M",
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        dt_input = ["%Y-%m-%dT%H:%M", "%Y-%m-%d %H:%M:%S", "%Y-%m-%d %H:%M"]
+        self.fields["deadline"].input_formats = dt_input
+        self.fields["race_datetime"].input_formats = dt_input
+        self.fields["deadline"].help_text = "Точная дата и время закрытия предиктов."
+
 
 class EventPhotoInline(admin.TabularInline):
     model = EventPhoto
@@ -13,10 +38,12 @@ class ResultInline(admin.StackedInline):
 
 @admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
+    form = EventAdminForm
     list_display = ("round_number", "name", "status", "deadline")
     list_filter = ("status",)
     search_fields = ("name",)
     inlines = [EventPhotoInline, ResultInline]
+    fields = ("name", "round_number", "status", "deadline", "race_datetime", "cover_image")
 
     actions = ["recalculate_scores"]
 
